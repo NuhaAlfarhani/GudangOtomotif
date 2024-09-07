d5cfac1672763146195d1ef6e91fd40b05079ef0
NuhaAlfarhani <muhammad.difa.21@student.se.ittelkom-sby.ac.id>
2024-09-07 18:37:09 +0700

    add fa connection

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -19,6 +19,7 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://code.jquery.com/ui/1.13.0/jquery-ui.min.js"></script>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="sb-nav-fixed">
 
--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,8 @@ export default defineConfig({
                 'resources/css/app.css',
                 'resources/js/app.js',
             ],
+
+            refresh: true,
         }),
     ],
 });
