d5b998a1f180fc96af71a45a5d90b38d62b45c39
NuhaAlfarhani <muhammad.difa.21@student.se.ittelkom-sby.ac.id>
2024-10-18 23:45:35 +0700

    Refactor header.blade.php to fix script and link tags

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -8,30 +8,28 @@
     <link rel ="icon" type="image/png/jpg" href="{{ asset('assets')}}/images/Hino-logo-2048x2048.png">
 
     <link rel="stylesheet" type="text/css" href="{{ asset('assets') }}/css/index-style.css">
-    <link type="text/javascript" src="{{ asset('assets') }}/js/bootstrap.js">
-    <link type="text/javascript" src="{{ asset('assets') }}/js/jquery-3.2.1.min.js">
+    <script src="{{ asset('assets') }}/js/jquery-3.2.1.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/js/all.min.js" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/js/all.min.js" crossorigin="anonymous"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://code.jquery.com/ui/1.13.0/jquery-ui.min.js"></script>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
 </head>
+
 <body class="sb-nav-fixed">
-
+    
     @include('components.navbar')
-
+    
     <div id="layoutSidenav">
         @include('components.sidenav')
         
         @yield('content')
     </div>
-
+    
     @include('layouts.footer')
-
+    
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </body>
 </html>
